c241a760e390509343560f527f0451926f547790
Khoa Tra <khoadtra@gmail.com>
2024-08-07 01:08:44 -0700

    reformatted pages 3

--- src/app/generate/page.js
+++ src/app/generate/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      <h1 className="text-center text-lg pt-4">Generate PC page</h1>
+      <h1 className="text-center text-lg pt-4">Generate PC Page</h1>
     </>
   );
 }
