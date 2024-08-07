3f3107fd47763481ed5c77cb545a6cb0851bb026
Khoa Tra <quaack.uw.edu>
2024-08-07 00:49:02 -0700

    Reformmated pages 2

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      <h1 className="text-center text-lg pt-4">home page</h1>
+      <h1 className="text-center text-lg pt-4">Home page</h1>
     </>
   );
 }
